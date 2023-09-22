d2fdbdfb0797f6664ad58cd5c89caa12389026b4
Carl Lundin <clundin@google.com>
2023-09-21 17:01:22 -0700

    Update OpenSSL version.

--- cryptography-linux/openssl-version.sh
+++ cryptography-linux/openssl-version.sh
@@ -1,5 +1,5 @@
-export OPENSSL_VERSION="openssl-1.1.1u"
-export OPENSSL_SHA256="e2f8d84b523eecd06c7be7626830370300fbcc15386bf5142d72758f6963ebc6"
+export OPENSSL_VERSION="openssl-1.1.1w"
+export OPENSSL_SHA256="cf3098950cb4d853ad95c0841f1f9c6d3dc102dccfcacd521d93925208b76ac8"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
 export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine no-apps no-docs no-sm2-precomp"
