8c11c9c4e2ac1982bcaa8c9a9db97de28db3c27a
Carl Lundin <clundin@google.com>
2024-06-04 14:33:21 -0700

    Try to fix windows build error.

--- cryptography-linux/openssl-version.sh
+++ cryptography-linux/openssl-version.sh
@@ -2,5 +2,5 @@ export OPENSSL_VERSION="openssl-3.1.5"
 export OPENSSL_SHA256="6ae015467dabf0469b139ada93319327be24b98251ffaeceda0221848dc09262"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
-export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine no-apps no-docs no-sm2-precomp"
+export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine"
 export OPENSSL_BUILD_FLAGS="${OPENSSL_BUILD_FLAGS_WINDOWS} enable-ec_nistp_64_gcc_128"
