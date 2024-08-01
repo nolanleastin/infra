efe9598393b90e5918b6f55056e10723225b90eb
kevin <32066188+kevinkngo@users.noreply.github.com>
2024-08-01 14:26:23 +1000

    Use openssl-3.1.6 to align with Piper
    Also added some changes needed for GitHub action to work

--- cryptography-linux/openssl-version.sh
+++ cryptography-linux/openssl-version.sh
@@ -1,5 +1,5 @@
-export OPENSSL_VERSION="openssl-3.1.5"
-export OPENSSL_SHA256="6ae015467dabf0469b139ada93319327be24b98251ffaeceda0221848dc09262"
+export OPENSSL_VERSION="openssl-3.1.6"
+export OPENSSL_SHA256="5d2be4036b478ef3cb0a854ca9b353072c3a0e26d8a56f8f0ab9fb6ed32d38d7"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
 export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine"
